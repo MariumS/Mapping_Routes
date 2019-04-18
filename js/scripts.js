462ce4421c95a9f5df444414035fffc9cf243c41
Marium Sultan <mas1300@nyu.edu>
2019-04-18 15:32:00 -0400

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,140 @@
-
-// sets up my mapbox access token so they can track my usage of their basemap services
 mapboxgl.accessToken = 'pk.eyJ1IjoibWFyemlwYW45NCIsImEiOiJjanVrOTdwaDQxdG42NDRwNGFmbzY5dWdtIn0.4lVQxPc89QYzHas2IIWmew';
-
 var map = new mapboxgl.Map({
-  container: 'mapContainer',
-  style: 'mapbox://styles/mapbox/light-v9',
-  center: [-73.98, 40.70],
-  zoom: 9.5,
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: [-96, 37.8],
+  zoom: 3
 });
 
-// Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
+travel_data.forEach(function(travels) {
+origin = ([travels.Lon[travel_data], travels.Lat[travel_data]]);
+destination = ([travels.Lon[travel_data+1], travels.Lat[travel_data+1]]);
 
-studentPizzaShops.forEach(function(StudentData) {
+// A simple line from origin to destination.
+var route = {
+  "type": "FeatureCollection",
+  "features": [{
+    "type": "Feature",
+    "geometry": {
+      "type": "LineString",
+      "coordinates": [
+        origin,
+        destination
+      ]
+    }
+  }]
+};
 
-  var thisStudentsColor = 'steelblue';
-  if (StudentData.nyuprogram === 'wagner') thisStudentsColor = 'orange';
-  if (StudentData.nyuprogram === 'cusp') thisStudentsColor = 'purple';
-  if (StudentData.nyuprogram === 'adjunct') thisStudentsColor = 'green';
-  if (StudentData.nyuprogram === 'global liberal studies') thisStudentsColor = 'yellow';
+// A single point that animates along the route.
+// Coordinates are initially set to origin.
+var point = {
+  "type": "FeatureCollection",
+  "features": [{
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+      "type": "Point",
+      "coordinates": origin
+    }
+  }]
+};
 
-  new mapboxgl.Marker({
-    color: thisStudentsColor,
-  })
-    .setLngLat([StudentData.lng, StudentData.lat])
-    .setPopup(new mapboxgl.Popup({ offset: 10 })
-    .setText(`${StudentData.name} says their favorite pizza shop is ${StudentData.favoritepizzashop}`))
-    .addTo(map);
+// Calculate the distance in kilometers between route start/end point.
+var lineDistance = turf.lineDistance(route.features[0], 'kilometers');
+
+var arc = [];
+
+// Number of steps to use in the arc and animation, more steps means
+// a smoother arc and animation, but too many steps will result in a
+// low frame rate
+var steps = 500;
+
+// Draw an arc between the `origin` & `destination` of the two points
+for (var i = 0; i < lineDistance; i += lineDistance / steps) {
+  var segment = turf.along(route.features[0], i, 'kilometers');
+  arc.push(segment.geometry.coordinates);
+}
+
+// Update the route with calculated arc coordinates
+route.features[0].geometry.coordinates = arc;
+
+// Used to increment the value of the point measurement against the route.
+var counter = 0;
+
+map.on('load', function() {
+  // Add a source and layer displaying a point which will be animated in a circle.
+  map.addSource('route', {
+    "type": "geojson",
+    "data": route
+  });
+
+  map.addSource('point', {
+    "type": "geojson",
+    "data": point
+  });
+
+  map.addLayer({
+    "id": "route",
+    "source": "route",
+    "type": "line",
+    "paint": {
+      "line-width": 2,
+      "line-color": "#007cbf"
+    }
+  });
+
+  map.addLayer({
+    "id": "point",
+    "source": "point",
+    "type": "symbol",
+    "layout": {
+      "icon-image": "airport-15",
+      "icon-rotate": ["get", "bearing"],
+      "icon-rotation-alignment": "map",
+      "icon-allow-overlap": true,
+      "icon-ignore-placement": true
+    }
+  });
+
+  function animate() {
+    // Update point geometry to a new position based on counter denoting
+    // the index to access the arc.
+    point.features[0].geometry.coordinates = route.features[0].geometry.coordinates[counter];
+
+    // Calculate the bearing to ensure the icon is rotated to match the route arc
+    // The bearing is calculate between the current point and the next point, except
+    // at the end of the arc use the previous point and the current point
+    point.features[0].properties.bearing = turf.bearing(
+      turf.point(route.features[0].geometry.coordinates[counter >= steps ? counter - 1 : counter]),
+      turf.point(route.features[0].geometry.coordinates[counter >= steps ? counter : counter + 1])
+    );
+
+    // Update the source with this new data.
+    map.getSource('point').setData(point);
+
+    // Request the next frame of animation so long the end has not been reached.
+    if (counter < steps) {
+      requestAnimationFrame(animate);
+    }
+
+    counter = counter + 1;
+  }
+
+  document.getElementById('replay').addEventListener('click', function() {
+    // Set the coordinates of the original point back to origin
+    point.features[0].geometry.coordinates = origin;
+
+    // Update the source layer
+    map.getSource('point').setData(point);
+
+    // Reset the counter
+    counter = 0;
+
+    // Restart the animation.
+    animate(counter);
+  });
+
+  // Start the animation.
+  animate(counter);
+});
 });
