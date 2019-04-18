c4893702e06cf77dd9b759d969157e5c3ed66fea
Marium Sultan <mas1300@nyu.edu>
2019-04-18 17:08:24 -0400

    the popupts

---
+++ js/travels.js
@@ -0,0 +1,63 @@
+var travel_data = [
+  {
+    "Location": "Kansas",
+    "Lon": -98.4842,
+    "Lat": 39.0119,
+    "Pop_Up": "was raised on a farm"
+  },
+  {
+    "Location": "DC",
+    "Lon": -77.03637,
+    "Lat": 38.89511,
+    "Pop_Up": "joined Department of Agriculture heading the Office of Plant and Seed Introduction"
+  },
+  {
+    "Location": "Naples",
+    "Lon": 14.305573,
+    "Lat": 40.853294,
+    "Pop_Up": "studied algae and termites"
+  },
+  {
+    "Location": "Bavaria",
+    "Lon": 11.4979,
+    "Lat": 48.7904,
+    "Pop_Up": "collected Hops to make American beer better tasting"
+  },
+  {
+    "Location": "Austria-Hungry",
+    "Lon": 16.3738,
+    "Lat": 48.2082,
+    "Pop_Up": "collected Kale, a staple food of the poor"
+  },
+  {
+    "Location": "Java",
+    "Lon": 113.9213,
+    "Lat": 0.7893,
+    "Pop_Up": "collected Mangosteens, which he decleared his favorite fruit"
+  },
+  {
+    "Location": "Egypt",
+    "Lon": 26.8206,
+    "Lat": 30.8025,
+    "Pop_Up": "collected Egyptian Cotton, softer than the cotton previously grown in America"
+  },
+  {
+    "Location": "Peru",
+    "Lon": -77.042793,
+    "Lat":  -12.046374,
+    "Pop_Up": "collected Quinoa, a crop which didn't gain popularity until 100 years later"
+  },
+  {
+    "Location": "Japan",
+    "Lon": 135.497009,
+    "Lat": 34.669529,
+    "Pop_Up": "collected Cherry Blossom Trees, prompting the government to request 1000 more from Japan"
+  },
+  {
+    "Location": "Florida",
+    "Lon": 27.6648,
+    "Lat": -81.5158,
+    "Pop_Up": "settled down with his family at the end of his travels"
+  }
+]
+;
